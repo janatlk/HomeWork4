99a9823fcdaded9a557c92a752cadfb49a1902ee
janatlk <janat00991@gmail.com>
2022-04-22 18:15:02 +0600

    Homework

--- homework5/hw5_janat.py
+++ homework5/hw5_janat.py
@@ -1,11 +1,15 @@
 import re
 
 class ValidCarNumber:
-    def is_valid(self,number):
-        isvalid = re.search(r"(01|02|03)(KG|RU|KZ)([0-9]{3})(BMW|TYT|LEX)",number)
-        if isvalid == None:
-            print("Невалидный номер")
-        else:
+    def __init__(self,number):
+        self.number = number
+    def is_valid(self):
+        isvalid = re.search(r"(01|02|03)(KG|RU|KZ)([0-9]{3})(BMW|TYT|LEX)",self.number)
+        # if isvalid == None:
+        #     print("Невалидный номер")
+        if isvalid and self.number == self.number[isvalid.start():isvalid.end()]:
             print("Валидный номер")
-num = ValidCarNumber.is_valid(0,"02KG777BMW")
-num2 = ValidCarNumber.is_valid(0,"hhh777hhh")
+        else:
+            print('Невалидный номер')
+num = ValidCarNumber('01KG777BMW')
+num.is_valid()
