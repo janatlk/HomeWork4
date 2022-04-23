57bd1ee99f372ce16539ecdd8d85d0194bd02e69
janatlk <janat00991@gmail.com>
2022-04-23 22:12:40 +0600

    Some changes

--- extratask(5).py
+++ extratask(5).py
@@ -1,4 +1,14 @@
-x = "Hello my name is Steven"
-x = x.split()
-list1 = list(x[-1])
-print(len(list1))
+import re
+class Sentence:
+    def __init__(self,sentence):
+        self.sentence = sentence
+    def split1(self):
+        x = re.split('[ ]',self.sentence)
+        '''В [скобках] можно указать любой символ который будет разделять слова'''
+        print(x[-1])
+        lst = list(x[-1])
+        print(len(lst))
+
+
+sen = Sentence("Pleae,eat this strawberries")
+sen.split1()
